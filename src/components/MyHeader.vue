ba592ad3a9962e1a70a179f1b0aecddc5c713684
Yaroslav Kovalchuk <dantekip972@gmail.com>
2023-10-18 01:03:57 +0300

    fix mobile header

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -41,10 +41,13 @@ const mobileMenuOpen = ref(false)
     </nav>
     <Dialog as="div" class="lg:hidden" @close="mobileMenuOpen = false" :open="mobileMenuOpen">
       <div class="fixed inset-0 z-10" />
-      <DialogPanel class="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-orange-600/75 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+      <DialogPanel
+          @click="mobileMenuOpen = false"
+          class="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-orange-400/90 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
         <div class="flex items-center justify-between">
-          <a href="#" class="-m-1.5 p-1.5">
-            <span class="sr-only">NIX</span>
+          <a class="-m-1.5 p-1.5 flex flex-col items-center">
+            <span class="logo text-3xl font-extrabold text-black">NIX</span>
+            <span class="logo text-2xl font-bold text-black">-workout-</span>
           </a>
           <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700" @click="mobileMenuOpen = false">
             <span class="sr-only">Close menu</span>
