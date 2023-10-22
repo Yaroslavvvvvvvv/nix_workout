39524cf3f4efddf06f577fd0e3b38219699471a2
Yaroslav Kovalchuk <dantekip972@gmail.com>
2023-10-22 21:58:19 +0300

    add some navigation

--- src/components/AboutPage.vue
+++ src/components/AboutPage.vue
@@ -11,7 +11,7 @@
         Calisthenics | Street workout brand
       </span>
     </div>
-    <div class="sticky top-0 h-screen flex flex-col items-center justify-center bg-orange-500/90">
+    <div class="sticky top-0 h-screen flex flex-col items-center justify-center bg-gray-300">
       <div class="container my-24 mx-auto md:px-6">
         <div class="justify-center flex-1 max-w-6xl py-4 mx-auto lg:py-6 md:px-6">
           <div class="flex flex-wrap">
@@ -23,7 +23,7 @@
               />
             </div>
             <div class="w-full px-4 mb-10 lg:w-1/2 lg:mb-0">
-              <h2 class="mb-4 text-4xl font-semibold text-blue-500 dark:text-gray-300">{title}</h2>
+              <h2 class="mb-4 text-4xl font-semibold text-blue-500 dark:text-gray-300">Турніки Настінні</h2>
               <p class="mb-10 text-base leading-7 text-gray-500 dark:text-gray-400 text-left">
                 {description}
               </p>
@@ -32,7 +32,7 @@
         </div>
       </div>
     </div>
-    <div class="sticky top-0 h-screen flex flex-col items-center justify-center bg-black text-white">
+    <div class="sticky top-0 h-screen flex flex-col items-center justify-center bg-gray-300 text-white">
       <div class="container my-24 mx-auto md:px-6">
         <div class="justify-center flex-1 max-w-6xl py-4 mx-auto lg:py-6 md:px-6">
           <div class="flex flex-wrap">
@@ -44,7 +44,7 @@
               />
             </div>
             <div class="w-full px-4 mb-10 lg:w-1/2 lg:mb-0">
-              <h2 class="mb-4 text-4xl font-semibold text-blue-500 dark:text-gray-300">{title}</h2>
+              <h2 class="mb-4 text-4xl font-semibold text-blue-500 dark:text-gray-300">Переносні Бруси</h2>
               <p class="mb-10 text-base leading-7 text-gray-500 dark:text-gray-400 text-left">
                 {description}
               </p>
@@ -53,8 +53,152 @@
         </div>
       </div>
     </div>
-    <div class="sticky top-0 h-screen flex flex-col items-center justify-center bg-purple-600 text-white">
-
+    <div class="sticky top-0 h-screen flex flex-col items-center justify-center bg-gray-300">
+      <div class="container my-24 mx-auto md:px-6">
+        <div class="justify-center flex-1 max-w-6xl py-4 mx-auto lg:py-6 md:px-6">
+          <div class="flex flex-wrap">
+            <div class="w-full px-4 mb-10 lg:w-1/2 lg:mb-0">
+              <img
+                  src="../assets/images/first.jpg"
+                  alt="img"
+                  class="relative z-40 object-cover w-full h-96 rounded-3xl"
+              />
+            </div>
+            <div class="w-full px-4 mb-10 lg:w-1/2 lg:mb-0">
+              <h2 class="mb-4 text-4xl font-semibold text-blue-500 dark:text-gray-300">Стійка для елементів</h2>
+              <p class="mb-10 text-base leading-7 text-gray-500 dark:text-gray-400 text-left">
+                {description}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="sticky top-0 h-screen flex flex-col items-center justify-center bg-gray-300 text-white">
+      <div class="container my-24 mx-auto md:px-6">
+        <div class="justify-center flex-1 max-w-6xl py-4 mx-auto lg:py-6 md:px-6">
+          <div class="flex flex-wrap">
+            <div class="w-full px-4 mb-10 lg:w-1/2 lg:mb-0">
+              <img
+                  src="../assets/images/fourth.png"
+                  alt="img"
+                  class="relative z-40 object-cover w-full h-96 rounded-3xl"
+              />
+            </div>
+            <div class="w-full px-4 mb-10 lg:w-1/2 lg:mb-0">
+              <h2 class="mb-4 text-4xl font-semibold text-blue-500 dark:text-gray-300">Паралетси</h2>
+              <p class="mb-10 text-base leading-7 text-gray-500 dark:text-gray-400 text-left">
+                {description}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="sticky top-0 h-screen flex flex-col items-center justify-center bg-gray-300">
+      <div class="container my-24 mx-auto md:px-6">
+        <div class="justify-center flex-1 max-w-6xl py-4 mx-auto lg:py-6 md:px-6">
+          <div class="flex flex-wrap">
+            <div class="w-full px-4 mb-10 lg:w-1/2 lg:mb-0">
+              <img
+                  src="../assets/images/first.jpg"
+                  alt="img"
+                  class="relative z-40 object-cover w-full h-96 rounded-3xl"
+              />
+            </div>
+            <div class="w-full px-4 mb-10 lg:w-1/2 lg:mb-0">
+              <h2 class="mb-4 text-4xl font-semibold text-blue-500 dark:text-gray-300">Гумові і латексні петлі</h2>
+              <p class="mb-10 text-base leading-7 text-gray-500 dark:text-gray-400 text-left">
+                {description}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="sticky top-0 h-screen flex flex-col items-center justify-center bg-gray-300 text-white">
+      <div class="container my-24 mx-auto md:px-6">
+        <div class="justify-center flex-1 max-w-6xl py-4 mx-auto lg:py-6 md:px-6">
+          <div class="flex flex-wrap">
+            <div class="w-full px-4 mb-10 lg:w-1/2 lg:mb-0">
+              <img
+                  src="../assets/images/fourth.png"
+                  alt="img"
+                  class="relative z-40 object-cover w-full h-96 rounded-3xl"
+              />
+            </div>
+            <div class="w-full px-4 mb-10 lg:w-1/2 lg:mb-0">
+              <h2 class="mb-4 text-4xl font-semibold text-blue-500 dark:text-gray-300">Обважнювачі</h2>
+              <p class="mb-10 text-base leading-7 text-gray-500 dark:text-gray-400 text-left">
+                {description}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="sticky top-0 h-screen flex flex-col items-center justify-center bg-gray-300">
+      <div class="container my-24 mx-auto md:px-6">
+        <div class="justify-center flex-1 max-w-6xl py-4 mx-auto lg:py-6 md:px-6">
+          <div class="flex flex-wrap">
+            <div class="w-full px-4 mb-10 lg:w-1/2 lg:mb-0">
+              <img
+                  src="../assets/images/first.jpg"
+                  alt="img"
+                  class="relative z-40 object-cover w-full h-96 rounded-3xl"
+              />
+            </div>
+            <div class="w-full px-4 mb-10 lg:w-1/2 lg:mb-0">
+              <h2 class="mb-4 text-4xl font-semibold text-blue-500 dark:text-gray-300">Шкіряні накладки</h2>
+              <p class="mb-10 text-base leading-7 text-gray-500 dark:text-gray-400 text-left">
+                {description}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="sticky top-0 h-screen flex flex-col items-center justify-center bg-gray-300 text-white">
+      <div class="container my-24 mx-auto md:px-6">
+        <div class="justify-center flex-1 max-w-6xl py-4 mx-auto lg:py-6 md:px-6">
+          <div class="flex flex-wrap">
+            <div class="w-full px-4 mb-10 lg:w-1/2 lg:mb-0">
+              <img
+                  src="../assets/images/fourth.png"
+                  alt="img"
+                  class="relative z-40 object-cover w-full h-96 rounded-3xl"
+              />
+            </div>
+            <div class="w-full px-4 mb-10 lg:w-1/2 lg:mb-0">
+              <h2 class="mb-4 text-4xl font-semibold text-blue-500 dark:text-gray-300">Кистьові бинти</h2>
+              <p class="mb-10 text-base leading-7 text-gray-500 dark:text-gray-400 text-left">
+                {description}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="sticky top-0 h-screen flex flex-col items-center justify-center bg-gray-300">
+      <div class="container my-24 mx-auto md:px-6">
+        <div class="justify-center flex-1 max-w-6xl py-4 mx-auto lg:py-6 md:px-6">
+          <div class="flex flex-wrap">
+            <div class="w-full px-4 mb-10 lg:w-1/2 lg:mb-0">
+              <img
+                  src="../assets/images/first.jpg"
+                  alt="img"
+                  class="relative z-40 object-cover w-full h-96 rounded-3xl"
+              />
+            </div>
+            <div class="w-full px-4 mb-10 lg:w-1/2 lg:mb-0">
+              <h2 class="mb-4 text-4xl font-semibold text-blue-500 dark:text-gray-300">Магнезія</h2>
+              <p class="mb-10 text-base leading-7 text-gray-500 dark:text-gray-400 text-left">
+                {description}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
     <div class="sticky top-0 h-screen flex flex-col items-center justify-center bg-neutral-800 text-white">
       <h2 class="text-4xl">The Fourth Title</h2>
