49b0fbb074cac533e8ba283cc1d9a3a1c95ca183
Yaroslav Kovalchuk <dantekip972@gmail.com>
2023-10-22 18:30:12 +0300

    create abput page

--- src/components/AboutPage.vue
+++ src/components/AboutPage.vue
@@ -12,26 +12,72 @@
     </div>
     <div class="sticky top-0 h-screen flex flex-col items-center justify-center bg-orange-400/90">
       <div class="container my-24 mx-auto md:px-6">
-        <section class="mb-32">
-          <div class="block rounded-lg">
-            <div class="flex flex-wrap items-center">
-              <div class="hidden grow-0 basis-auto lg:flex md:flex xl:w-4/12 lg:w-6/12 md:w-7/12 sm:w-4/12">
-                <img src="../assets/images/first.jpg" alt="image"
-                     class="w-full rounded-t-lg rounded-b-lg  lg:rounded-bl-lg"/>
-              </div>
-              <div class="w-full shrink-0 grow-0 basis-auto xl:w-8/12 lg:w-6/12 md:w-5/12">
-                <div class="px-6 py-12 md:px-12 text-center">
-                  <h1 class="text-3xl font-extrabold xl:text-6xl lg:text-4xl md:text-xl">
-                    Бренд спортивного обладнання
-                  </h1>
-                </div>
-              </div>
+        <div class="justify-center flex-1 max-w-6xl py-4 mx-auto lg:py-6 md:px-6">
+          <div class="flex flex-wrap">
+            <div class="w-full px-4 mb-10 lg:w-1/2 lg:mb-0">
+              <img
+                  src="../assets/images/second.jpg"
+                  alt="img"
+                  class="relative z-40 object-cover w-full h-96 rounded-3xl"
+              />
+            </div>
+            <div class="w-full px-4 mb-10 lg:w-1/2 lg:mb-0">
+              <h2 class="mb-4 text-4xl font-semibold text-blue-500 dark:text-gray-300">{title}</h2>
+              <p class="mb-10 text-base leading-7 text-gray-500 dark:text-gray-400 text-left">
+                {description}
+              </p>
             </div>
           </div>
-        </section>
+        </div>
       </div>
     </div>
+    <div class="sticky top-0 h-screen flex flex-col items-center justify-center bg-orange-400 text-white">
+      <div class="container my-24 mx-auto md:px-6">
+        <div class="justify-center flex-1 max-w-6xl py-4 mx-auto lg:py-6 md:px-6">
+          <div class="flex flex-wrap">
+            <div class="w-full px-4 mb-10 lg:w-1/2 lg:mb-0">
+              <img
+                  src="../assets/images/first.jpg"
+                  alt="img"
+                  class="relative z-40 object-cover w-full h-96 rounded-3xl"
+              />
+            </div>
+            <div class="w-full px-4 mb-10 lg:w-1/2 lg:mb-0">
+              <h2 class="mb-4 text-4xl font-semibold text-blue-500 dark:text-gray-300">{title}</h2>
+              <p class="mb-10 text-base leading-7 text-gray-500 dark:text-gray-400 text-left">
+                {description}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="sticky top-0 h-screen flex flex-col items-center justify-center bg-orange-400 text-white">
+      <div class="container my-24 mx-auto md:px-6">
+        <div class="justify-center flex-1 max-w-6xl py-4 mx-auto lg:py-6 md:px-6">
+          <div class="flex flex-wrap">
+            <div class="w-full px-4 mb-10 lg:w-1/2 lg:mb-0">
+              <img
+                  src="../assets/images/fourth.png"
+                  alt="img"
+                  class="relative z-40 object-cover w-full h-96 rounded-3xl"
+              />
+            </div>
+            <div class="w-full px-4 mb-10 lg:w-1/2 lg:mb-0">
+              <h2 class="mb-4 text-4xl font-semibold text-blue-500 dark:text-gray-300">{title}</h2>
+              <p class="mb-10 text-base leading-7 text-gray-500 dark:text-gray-400 text-left">
+                {description}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="sticky top-0 h-screen flex flex-col items-center justify-center bg-neutral-800 text-white">
+      <h2 class="text-4xl">The Fourth Title</h2>
+    </div>
   </div>
+
 </template>
 
 <style scoped>
