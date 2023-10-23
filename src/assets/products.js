d983ee5754a2470b73ca93eb2b961d9ff25106a1
Yaroslav Kovalchuk <dantekip972@gmail.com>
2023-10-23 14:29:53 +0300

    fix error for img

---
+++ src/assets/products.js
@@ -0,0 +1,56 @@
+export const products = [
+    {
+        id: 1,
+        name: 'Турніки Настінні',
+        about: '',
+        imageSrc: '../src/assets/images/products/bars.jpg',
+    },
+    {
+        id: 2,
+        name: 'Переносні Бруси',
+        about: '',
+        imageSrc: '../src/assets/images/products/low_bars.jpg',
+    },
+    {
+        id: 3,
+        name: 'Стійка для елементів',
+        about: '',
+        imageSrc: '../src/assets/images/products/stand.jpg',
+    },
+    {
+        id: 4,
+        name: 'Паралетси',
+        about: '',
+        imageSrc: '../src/assets/images/products/paralets.jpg',
+    },
+    {
+        id: 5,
+        name: 'Гумові і латексні петлі',
+        about: '',
+        imageSrc: '../src/assets/images/products/rubber.jpg',
+    },
+    {
+        id: 6,
+        name: 'Обважнювачі',
+        about: '',
+        imageSrc: '../src/assets/images/products/gainers.jpg',
+    },
+    {
+        id: 7,
+        name: 'Шкіряні накладки',
+        about: '',
+        imageSrc: '../src/assets/images/products/linings.jpg',
+    },
+    {
+        id: 8,
+        name: 'Кистьові бинти',
+        about: '',
+        imageSrc: '../src/assets/images/products/bandages.jpg',
+    },
+    {
+        id: 9,
+        name: 'Магнезія',
+        about: '',
+        imageSrc: '../src/assets/images/products/magnesium.jpg',
+    },
+]
