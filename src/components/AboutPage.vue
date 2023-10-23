0b0458498973de092b8de791cca3bfc2a06922e7
Yaroslav Kovalchuk <dantekip972@gmail.com>
2023-10-23 14:44:19 +0300

    try fix images

--- src/components/AboutPage.vue
+++ src/components/AboutPage.vue
@@ -1,5 +1,60 @@
 <script setup>
-import {products} from "../assets/products.js";
+const products = [
+  {
+    id: 1,
+    name: 'Турніки Настінні',
+    about: '',
+    imageSrc: '../src/assets/images/bars.jpg',
+  },
+  {
+    id: 2,
+    name: 'Переносні Бруси',
+    about: '',
+    imageSrc: '../src/assets/images/low_bars.jpg',
+  },
+  {
+    id: 3,
+    name: 'Стійка для елементів',
+    about: '',
+    imageSrc: '../src/assets/images/stand.jpg',
+  },
+  {
+    id: 4,
+    name: 'Паралетси',
+    about: '',
+    imageSrc: '../src/assets/images/paralets.jpg',
+  },
+  {
+    id: 5,
+    name: 'Гумові і латексні петлі',
+    about: '',
+    imageSrc: '../src/assets/images/rubber.jpg',
+  },
+  {
+    id: 6,
+    name: 'Обважнювачі',
+    about: '',
+    imageSrc: '../src/assets/images/gainers.jpg',
+  },
+  {
+    id: 7,
+    name: 'Шкіряні накладки',
+    about: '',
+    imageSrc: '../src/assets/images/linings.jpg',
+  },
+  {
+    id: 8,
+    name: 'Кистьові бинти',
+    about: '',
+    imageSrc: '../src/assets/images/bandages.jpg',
+  },
+  {
+    id: 9,
+    name: 'Магнезія',
+    about: '',
+    imageSrc: '../src/assets/images/magnesium.jpg',
+  },
+]
 </script>
 
 <template>
