d1346bca811c44d740e185fb5db0081251e270bd
Yaroslav Kovalchuk <dantekip972@gmail.com>
2023-10-26 16:40:38 +0300

    cgange styles in catalog component

--- src/views/MyCatalog.vue
+++ src/views/MyCatalog.vue
@@ -3,7 +3,7 @@
     <div class="flex justify-center items-center">
       <div class="2xl:mx-auto 2xl:container py-6 bg-stone-100 px-4 sm:px-6 xl:px-20 2xl:px-0 w-full">
         <div class="flex flex-col jusitfy-center items-center space-y-10">
-          <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 md:gap-x-4 md:gap-x-8 w-full">
+          <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 md:gap-x-4  w-full">
               <div class="relative group flex justify-center items-center h-full w-full">
                 <img class="object-center object-cover h-full w-full"
                      src="/images/catalog/men_bars.jpg" alt="men"/>
@@ -88,7 +88,7 @@
     <div class="flex justify-center items-center">
       <div class="2xl:mx-auto 2xl:container py-6 bg-stone-100 px-4 sm:px-6 xl:px-20 2xl:px-0 w-full">
         <div class="flex flex-col jusitfy-center items-center space-y-10">
-          <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 md:gap-x-4 md:gap-x-8 w-full">
+          <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 md:gap-x-4  w-full">
               <div class="relative group flex justify-center items-center h-full w-full">
                 <img class="object-center object-cover h-full w-full"
                      src="/images/catalog/men_stand.jpg" alt="girl-image"/>
