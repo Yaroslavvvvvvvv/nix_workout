aaed5d0c267794f6d155958d2b821c3b648ffe8d
Yaroslav Kovalchuk <dantekip972@gmail.com>
2023-10-28 18:21:39 +0300

    fix bg in header

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -17,7 +17,7 @@ const mobileMenuOpen = ref(false)
 </script>
 
 <template>
-  <header class="sticky top-0 bg-gray-950/95 z-50">
+  <header class="sticky top-0 bg-gray-950/95  z-50">
     <nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8 animate-fade-down animate-duration-1000"
          aria-label="Global">
       <div class="flex lg:flex-1">
@@ -33,7 +33,7 @@ const mobileMenuOpen = ref(false)
           <Bars3Icon class="h-6 w-6" aria-hidden="true" />
         </button>
       </div>
-      <PopoverGroup class="hidden md:flex lg:gap-x-12">
+      <PopoverGroup class="hidden lg:flex lg:gap-x-12">
         <router-link to="/about" class="text-lg font-bold leading-6 text-gray-300 hover:text-white">Про нас</router-link>
         <router-link to="/catalog" class="text-lg font-bold leading-6 text-gray-300 hover:text-white">Каталог</router-link>
         <router-link to="/delivery" class="text-lg font-bold leading-6 text-gray-300 hover:text-white">Доставка і оплата</router-link>
@@ -44,19 +44,19 @@ const mobileMenuOpen = ref(false)
       <div class="fixed inset-0 z-10" />
       <DialogPanel
           @click="mobileMenuOpen = false"
-          class="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-gray-100/75 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+          class="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-gray-100/90 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
         <div class="flex items-center justify-between">
           <a class="-m-1.5 p-1.5 flex flex-col items-center">
             <span class="logo text-3xl font-extrabold text-white">NIX</span>
             <span class="logo text-2xl font-bold text-white">-workout-</span>
           </a>
-          <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700" @click="mobileMenuOpen = false">
+          <button type="button" class="-m-2.5 rounded-md p-2.5" @click="mobileMenuOpen = false">
             <span class="sr-only">Close menu</span>
             <XMarkIcon class="h-6 w-6" aria-hidden="true" />
           </button>
         </div>
         <div class="mt-6 flow-root">
-          <div class="-my-6 divide-y divide-gray-500/10">
+          <div class="-my-6 divide-y divide-gray-500">
             <div class="space-y-2 py-6">
               <router-link to="/about" class="-mx-3 block rounded-lg px-3 py-2 text-base font-bold leading-7 text-gray-900 hover:bg-gray-100/50">Про нас</router-link>
               <router-link to="/catalog" class="-mx-3 block rounded-lg px-3 py-2 text-base font-bold leading-7 text-gray-900 hover:bg-gray-100/50">Каталог</router-link>
