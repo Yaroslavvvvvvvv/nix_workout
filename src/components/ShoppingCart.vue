b8787a8a44a21a039966b54a52d2d936efd43b51
Yaroslav Kovalchuk <dantekip972@gmail.com>
2023-11-03 17:30:51 +0200

    add some styles in cart

--- src/components/ShoppingCart.vue
+++ src/components/ShoppingCart.vue
@@ -11,13 +11,14 @@
             </div>
             <div class="mt-4 flex justify-between im sm:space-y-6 sm:mt-0 sm:block sm:space-x-6">
               <div class="flex items-center border-gray-100">
-                <span class="cursor-pointer rounded-l bg-gray-100 py-1 px-3.5 duration-100 hover:bg-blue-500 hover:text-blue-50"> - </span>
+                <span class="cursor-pointer rounded-l bg-gray-100 py-1 px-3.5 duration-100 hover:bg-gray-500 hover:text-blue-50"> - </span>
                 <input class="h-8 w-8 border bg-white text-center text-xs outline-none" type="number" value="2" min="1" />
-                <span class="cursor-pointer rounded-r bg-gray-100 py-1 px-3 duration-100 hover:bg-blue-500 hover:text-blue-50"> + </span>
+                <span class="cursor-pointer rounded-r bg-gray-100 py-1 px-3 duration-100 hover:bg-gray-500 hover:text-blue-50"> + </span>
               </div>
               <div class="flex items-center space-x-4">
-                <p class="text-sm">{{product.price}} грн</p>
-                <svg @click="removeItem(product)" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-5 w-5 cursor-pointer duration-150 hover:text-red-500">
+                <p class="text-lg font-medium">{{product.price}} грн</p>
+                <svg @click="removeItem(product)" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                     stroke-width="1.5" stroke="currentColor" class="h-5 w-5 cursor-pointer duration-150 hover:text-gray-500">
                   <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12"/>
                 </svg>
               </div>
@@ -30,12 +31,12 @@
 
         <hr class="my-4" />
         <div class="flex justify-between">
-          <p class="text-lg font-bold">Total</p>
-          <div class="">
+          <p class="text-lg font-bold">Загалом</p>
+          <div class="font-bold">
             <p class="mb-1 text-lg font-bold">{{totalPrice}} грн</p>
           </div>
         </div>
-        <button class="mt-6 w-full rounded-md bg-blue-500 py-1.5 font-medium text-blue-50 hover:bg-blue-600">Check out</button>
+        <button class="mt-6 w-full rounded-md bg-gray-600 py-1.5 font-medium text-blue-50 hover:bg-gray-800">Оформити</button>
       </div>
     </div>
   </div>
