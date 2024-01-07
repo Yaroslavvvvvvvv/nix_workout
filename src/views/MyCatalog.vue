41aea1f063203738db455e6b4a6d49ab0a1734f9
Yaroslav Kovalchuk <dantekip972@gmail.com>
2024-01-08 00:11:22 +0200

    fix error in my catalog

--- src/views/MyCatalog.vue
+++ src/views/MyCatalog.vue
@@ -133,7 +133,7 @@
               <router-link to="/magnesium">
                 <div class="relative group flex justify-center items-center h-full w-full">
                   <img class="object-center object-cover h-full w-full"
-                       src="/images/products/magnesium_img.jpg" alt="image"/>
+                       src="/images/products/magnesium_img.webp" alt="image"/>
                   <button
                       class="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-400 bottom-4 z-10
                     absolute text-base font-medium leading-none text-gray-800 py-3 w-36 bg-white">
